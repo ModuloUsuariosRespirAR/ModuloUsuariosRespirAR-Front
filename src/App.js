b00868d4717b7cadbff00657af2e7fcd7b9cdc74
neduardoaguire <neduardoaguirre@gmail.com>
2023-05-14 22:35:31 -0300

    main layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Users from './components/users/Users';
+import EditUser from './components/users/EditUser';
+import AddUser from './components/users/AddUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/users' element={<Users />} />
+        <Route exact path='/users/edit/:id' element={<EditUser />} />
+        <Route exact path='/users/add-user' element={<AddUser />} />
+      </Routes>
+    </Router>
   );
 }
 
